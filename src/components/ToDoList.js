9eb15cd10347812a0f50a30263b65593f5a5bcfa
Ramir <nasyrov.ramir@mail.ru>
2022-04-14 15:52:52 +0600

    Add redux and columns

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,15 +1,22 @@
 import { ToDoItem } from "./ToDoItem";
 
+<<<<<<< Updated upstream
 export function ToDoList({ todos, onRemove, dates }) {
+=======
+export function ToDoList({ todos, onTodoChange }) {
+  const sortedTodos = useMemo(() => {
+    const s = [...todos];
+    const completedTodos = s.filter((item) => item.done);
+    const currentTodos = s.filter((item) => !item.done);
+    return { completedTodos, currentTodos };
+  }, [todos]);
+
+>>>>>>> Stashed changes
   return (
-    <ul
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        marginTop: 20,
-        alignItems: "center",
-      }}
+    <div
+      style={{ display: "flex", gap: "25px", justifyContent: "space-around" }}
     >
+<<<<<<< Updated upstream
       {todos?.map((todo, index) => (
         <ToDoItem
           todo={todo}
@@ -19,5 +26,44 @@ export function ToDoList({ todos, onRemove, dates }) {
         />
       ))}
     </ul>
+=======
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: 20,
+          alignItems: "center",
+          width: 1000,
+        }}
+      >
+        <h2>Need to complete</h2>
+        {sortedTodos?.currentTodos.map((todo) => (
+          <ToDoItem
+            todo={todo}
+            onTodoChange={onTodoChange}
+            key={todo.created} // index лучше не использовать
+          />
+        ))}
+      </ul>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: 20,
+          alignItems: "center",
+          width: 1000,
+        }}
+      >
+        <h2>Completed todos</h2>
+        {sortedTodos?.completedTodos.map((todo) => (
+          <ToDoItem
+            todo={todo}
+            onTodoChange={onTodoChange}
+            key={todo.created} // index лучше не использовать
+          />
+        ))}
+      </ul>
+    </div>
+>>>>>>> Stashed changes
   );
 }
--- src/pages/ToDoPage.js
+++ src/pages/ToDoPage.js
@@ -1,8 +1,14 @@
+<<<<<<< Updated upstream
 import React, { useEffect, useState } from "react";
+=======
+import React, { useCallback } from "react";
+>>>>>>> Stashed changes
 import { ToDoForm } from "../components/ToDoForm";
 import { ToDoList } from "../components/ToDoList";
+import { useDispatch, useSelector } from "react-redux";
 
 export function ToDoPage() {
+<<<<<<< Updated upstream
   const [todos, setTodos] = useState(
     localStorage.getItem("todos")
       ? localStorage.getItem("todos").split(",")
@@ -35,11 +41,37 @@ export function ToDoPage() {
     newDates.splice(index, 1);
     setDates(newDates);
   }
+=======
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  const handleCreate = useCallback(
+    (todo) => {
+      dispatch({ type: "todos/add", payload: todo });
+    },
+    [dispatch]
+  );
+
+  const handleTodoChange = useCallback(
+    (created, value) => {
+      dispatch({
+        type: "todos/doneChange",
+        payload: created,
+        value,
+      });
+    },
+    [dispatch]
+  );
+>>>>>>> Stashed changes
 
   return (
     <div>
       <ToDoForm onCreate={handleCreate} />
+<<<<<<< Updated upstream
       <ToDoList todos={todos} onRemove={handleRemove} dates={dates} />
+=======
+      <ToDoList todos={todos} onTodoChange={handleTodoChange} />
+>>>>>>> Stashed changes
     </div>
   );
 }
